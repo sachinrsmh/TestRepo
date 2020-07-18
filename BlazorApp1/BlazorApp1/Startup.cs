918362355c21ff564ecbbaf0aa06e5b84dc524b8
sachinrsmh <sachinrsmh@gmail.com>
2020-07-18 17:24:26 +0530

    Changed

--- BlazorApp1/BlazorApp1/Startup.cs
+++ BlazorApp1/BlazorApp1/Startup.cs
@@ -31,7 +31,7 @@ namespace BlazorApp1
             services.AddSingleton<WeatherForecastService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Just changed the file This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
